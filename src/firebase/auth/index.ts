7e59a766ab23887b30f9e4deb2826134eb507d5f
syson <au1gust8@gmail.com>
2024-05-14 00:51:53 +0900

    Feat: firebase auth파일 세팅

---
+++ src/firebase/auth/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import "./auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
